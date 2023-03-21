bc9cec9d08167d4d576e383672caa6d68ec0c833
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-21 15:18:31 +0630

    sign up user

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -10,15 +10,28 @@
 
 <script>
 import { ref } from 'vue'
+import {auth} from "../firebase/config"
 export default {
         setup(){
             let displayName = ref("")
             let email = ref("");
             let password = ref("");
-            let signUp = ()=>{
-                console.log(displayName.value, email.value, password.value)
+            let error = ref(null);
+            let signUp = async()=>{
+                // console.log(displayName.value, email.value, password.value)
+                try{
+                    let res = await auth.createUserWithEmailAndPassword(email.value, password.value)
+               
+               if(!res){
+                    throw new Error("could not create new user")
+               }
+                }catch(err){
+                    // console.log(err.message);
+                    error.value = err.message;
+                }
+                // console.log(res.user);
+                console.log(error.value)
             }
-
             return {displayName, email, password, signUp}
         }
 }
